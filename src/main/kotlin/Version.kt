83820c8528ded2978ba7ec18a97e69cdc9a3b4e1
yufu <yui.ogasawara@uzabase.com>
2025-04-13 18:00:17 +0900

    parse できるようになった

--- src/main/kotlin/Version.kt
+++ src/main/kotlin/Version.kt
@@ -1,7 +1,24 @@
 package org.example
 
-class Version {
-    fun isValid(version: String): Boolean {
-        return version.matches(Regex("^JDK[0-9]+u[0-9]+$"))
+// TODO: なんか「プログラムで正規表現使うのはいけない」って聞いた気がするけどどうなんだろう…
+data class Version(val version: String) {
+    companion object {
+        fun isValid(version: String): Boolean {
+            return version.matches("^JDK[0-9]+u[0-9]+$".toRegex())
+        }
+
+        fun parse(version: String): Version {
+            return Version(version)
+        }
+    }
+
+    fun familyNumber(): String {
+        return version
+            .removePrefix("JDK")
+            .replace("u[0-9]+".toRegex(), "")
+    }
+
+    fun updateNumber(): String {
+        return version.replace("JDK[0-9]+u".toRegex(), "")
     }
 }
--- src/test/kotlin/VersionTest.kt
+++ src/test/kotlin/VersionTest.kt
@@ -4,23 +4,43 @@ import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 
 class VersionTest {
-    val target = Version()
-
     @Nested
     inner class `validかどうか調べる(「JDK{数値}u{数値}」という形式のみがvalidである)` {
         @Test
         fun `「JDK7u40」はvalidである`() {
-            target.isValid("JDK7u40") shouldBeEqual true
+            Version.isValid("JDK7u40") shouldBeEqual true
         }
 
         @Test
         fun `「hoge」はvalidじゃない`() {
-            target.isValid("hoge") shouldBeEqual false
+            Version.isValid("hoge") shouldBeEqual false
         }
 
         @Test
         fun `「JDK7u9x」はvalidじゃない`() {
-            target.isValid("JDK7u9x") shouldBeEqual false
+            Version.isValid("JDK7u9x") shouldBeEqual false
+        }
+    }
+
+    @Nested
+    inner class parseする {
+        // TODO: これを通すためにテスト対象を data class にしたんだけど それでいーんだっけ・・・？
+        @Test
+        fun `「JDK7u40」を表すオブジェクトを返す`() {
+            val version = "JDK7u40"
+            Version.parse(version) shouldBeEqual Version(version)
+        }
+
+        @Test
+        fun バージョン番号からfamilyNumberを返す() {
+            val target = Version.parse("JDK7u40")
+            target.familyNumber() shouldBeEqual "7"
+        }
+
+        @Test
+        fun バージョン番号からupdateNumberを返す() {
+            val target = Version.parse("JDK7u40")
+            target.updateNumber() shouldBeEqual "40"
         }
     }
 }
