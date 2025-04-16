756d34b896fa4216f0baac8cac7bff7750b9a6cd
yufu <atashism@gmail.com>
2025-04-17 07:48:51 +0900

    ちょっと進めた

--- src/main/kotlin/JavaVersion.kt
+++ src/main/kotlin/JavaVersion.kt
@@ -1,14 +1,14 @@
 package org.example
 
 // TODO: なんか「プログラムで正規表現使うのはいけない」って聞いた気がするけどどうなんだろう…
-data class Version(val version: String) {
+data class JavaVersion(val version: String) {
     companion object {
         fun isValid(version: String): Boolean {
             return version.matches("^JDK[0-9]+u[0-9]+$".toRegex())
         }
 
-        fun parse(version: String): Version {
-            return if (isValid(version)) Version(version) else throw IllegalArgumentException()
+        fun parse(version: String): JavaVersion {
+            return if (isValid(version)) JavaVersion(version) else throw IllegalArgumentException()
         }
     }
 
@@ -20,5 +20,13 @@ data class Version(val version: String) {
 
     fun updateNumber(): String {
         return version.replace("JDK[0-9]+u".toRegex(), "")
+    }
+
+    fun lesserThan(other: JavaVersion): Boolean {
+        return this.updateNumber() < other.updateNumber()
+    }
+
+    fun greaterThan(other: JavaVersion): Boolean {
+        return this.updateNumber() > other.updateNumber()
     }
 }
--- src/test/kotlin/JavaVersionTest.kt
+++ src/test/kotlin/JavaVersionTest.kt
@@ -1,37 +1,37 @@
 import io.kotest.assertions.throwables.shouldThrowExactly
 import io.kotest.matchers.equals.shouldBeEqual
-import org.example.Version
+import org.example.JavaVersion
 import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 
-class VersionTest {
+class JavaVersionTest {
     @Nested
     inner class `validかどうか調べる(「JDK{数値}u{数値}」という形式のみがvalidである)` {
         @Test
         fun `「JDK7u40」はvalidである`() {
-            Version.isValid("JDK7u40") shouldBeEqual true
+            JavaVersion.isValid("JDK7u40") shouldBeEqual true
         }
 
         @Test
         fun `「hoge」はvalidじゃない`() {
-            Version.isValid("hoge") shouldBeEqual false
+            JavaVersion.isValid("hoge") shouldBeEqual false
         }
 
         @Test
         fun `「JDK7u9x」はvalidじゃない`() {
-            Version.isValid("JDK7u9x") shouldBeEqual false
+            JavaVersion.isValid("JDK7u9x") shouldBeEqual false
         }
     }
 
     @Nested
     inner class parseする {
         val version = "JDK7u40"
-        val target = Version.parse(version)
+        val target = JavaVersion.parse(version)
 
         // TODO: これを通すためにテスト対象を data class にしたんだけど それでいーんだっけ・・・？
         @Test
         fun `「JDK7u40」を表すオブジェクトを返す`() {
-            target shouldBeEqual Version(version)
+            target shouldBeEqual JavaVersion(version)
         }
 
         @Test
@@ -48,7 +48,7 @@ class VersionTest {
         @Test
         fun バージョン番号が不正な場合は例外を投げる() {
             shouldThrowExactly<IllegalArgumentException> {
-                Version.parse("JDK7u9x")
+                JavaVersion.parse("JDK7u9x")
             }
         }
     }
@@ -64,13 +64,19 @@ class VersionTest {
 //    #          ↑ 言語に応じて演算子オーバーロード等はご自由に
     @Nested
     inner class バージョンを大小比較する {
-        val u40 = Version.parse("JDK7u40")
-        val u51 = Version.parse("JDK7u51")
-        val jdk8u0 = Version.parse("JDK8u0")
+        val u40 = JavaVersion.parse("JDK7u40")
+        val u51 = JavaVersion.parse("JDK7u51")
+        val jdk8u0 = JavaVersion.parse("JDK8u0")
 
         @Test
-        fun `JDK7u40はJDK7u51より小さい`() {
-            TODO()
+        fun `「JDK7u40はJDK7u51より小さい」はtrue`() {
+            u40.lesserThan(u51) shouldBeEqual true
         }
+
+        @Test
+        fun `「JDK7u40はJDK7u51より大きい」はfalse`() {
+            u40.greaterThan(u51) shouldBeEqual false
+        }
+
     }
 }
